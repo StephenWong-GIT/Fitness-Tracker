6c682c1e374d9c0ba37b86c3d03f861e65ddf383
Stephen Wong <yummywongttv@gmail.com>
2021-11-13 16:11:27 -0500

    k

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useFindAndModify: false
 });
 
 app.use(apiRoutes);
